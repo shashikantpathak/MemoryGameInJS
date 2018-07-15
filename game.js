915cf963ffab9913e47cd8cd26ddfe2b7e76dcb6
Shashikant Pathak <35792373+shashikantpathak@users.noreply.github.com>
2018-07-15 20:18:00 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 let image = document.getElementsByClassName("image");
+// copy the image to new array using ES6
 let images = [...image]
 console.log(images);
 
-
+// get image by classname
 
 let matchedimage = document.getElementsByClassName("match");
-
+// define the shuffle method to shuffle the image items
 function shuffle(array) {
     var ctr = array.length;
     var temp;
@@ -23,7 +24,7 @@ function shuffle(array) {
 
 const patterns = document.getElementById("image-patterns");
 
-
+// start the game
 
 function startGame() {
     images = shuffle(images);
@@ -34,7 +35,7 @@ function startGame() {
         });
         images[i].classList.remove("show", "open", "match", "disabled");
     }
-
+// set the moves
     move = 0;
     counter.innerHTML = move;
 
@@ -42,7 +43,7 @@ function startGame() {
         stars[i].style.color = "Black";
         stars[i].style.visibility = "visible";
     }
-
+// set the timer
     second = 0;
     minute = 0;
     hour = 0;
@@ -50,7 +51,7 @@ function startGame() {
     timer.innerHTML = " 0:0:0s";
     clearInterval(interval);
 }
-
+// write the display image function to view images on click
 var displayimage = function() {
     this.classList.toggle("open");
     this.classList.toggle("show");
@@ -63,12 +64,12 @@ var displayimage = function() {
 
 
 var openimages = [];
-
+// check if images matched in pair
 function imageOpen() {
     openimages.push(this);
     var len = openimages.length;
     if (len == 2) {
-
+// call the counter function
         moveCounter();
 
 
@@ -84,7 +85,7 @@ function imageOpen() {
 
 
 
-
+// check if images are matched and set the state of images
 
 function matched() {
     openimages[0].classList.add("match", "disabled");
@@ -95,7 +96,7 @@ function matched() {
 }
 
 
-
+// check if the images are not matched and set the image states
 function unmatched() {
 
     openimages[0].classList.add("unmatched");
@@ -114,14 +115,14 @@ function unmatched() {
 
 }
 
-
+// disbale on click once image pair is found
 function disable() {
     Array.prototype.filter.call(images, function(image) {
         image.classList.add("disabled");
     });
 }
 
-
+// enable the  on click event if image pair is not matched
 
 
 function enable() {
@@ -137,7 +138,7 @@ function enable() {
 
 let move = 0;
 let counter = document.querySelector(".moves");
-
+// define the method for counting steps
 function moveCounter() {
     move++;
     counter.innerHTML = move;
@@ -165,7 +166,7 @@ function moveCounter() {
     }
 
 }
-
+// define the timer
 var second = 0;
 var minute = 0;
 var hour = 0;
@@ -191,7 +192,7 @@ function startTimer() {
 
 let endicon = document.querySelector(".end");
 let modal = document.getElementById("model_view");
-
+// open up the congratulatory message once the game is completed and display the status 
 function congratulations() {
     if (matchedimage.length == 16) {
         clearInterval(interval);
@@ -205,7 +206,7 @@ function congratulations() {
         endmodal();
     };
 }
-
+// exit the congratulatory display on click close button
 
 function endmodal() {
     endicon.addEventListener("click", function(e) {
@@ -213,7 +214,7 @@ function endmodal() {
         startGame();
     });
 }
-
+//  start the game again
 function playAgain() {
     modal.classList.remove("show");
     startGame();
@@ -223,7 +224,7 @@ const stars = document.querySelectorAll(".fa-star");
 
 let starslist = document.querySelectorAll(".stars");
 
-
+// lopp for different stages of game
 for (var i = 0; i < images.length; i++) {
     image = images[i];
     image.addEventListener("click", displayimage);
@@ -231,15 +232,12 @@ for (var i = 0; i < images.length; i++) {
     image.addEventListener("click", congratulations);
 };
 
-
+// play sound on click
 
 function PlaySound() {
     debugger
     var sound = document.getElementById("audio");
     sound.play()
 }
-
-
-
 
 startGame()
